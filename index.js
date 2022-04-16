9be7a939e30cf7f380a1b577e533c5a3786f035b
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-04-16 17:53:23 +0000

    updated code

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const Discord = require('discord.js');
-const {MessageAttachment}=require('discord.js');
 const {
-    Client,
-    Intents,
-  MessageEmbed, 
-  EnumResolvers, 
-  GatewayIntendBits
+    Client, Intents, Embed, Embedbuilder, EnumResolvers, GatewayIntendBits, Partials, ApplicationCommandType, ApplicationCommandOptionType, ButtonStyle, Colors, Collection, MessageEmbed, ButtonBuilder
 } = require('discord.js');
 const client = new Client({
-   partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER'], intents: [GatewayIntendBits.GUILDS, GatewayIntendBits.GUILD_MEMBERS, GatewayIntendBits.GUILD_MESSAGES, GatewayIntendBits.GUILD_MESSAGE_REACTIONS, "GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
+   partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.Guild_Member], intents: [GatewayIntendBits.Guilds, GatewayIntendBits.Guild_Members, GatewayIntendBits.Guild_Messages, GatewayIntendBits.Guild_Message_Reactions]
 });
 const keep_alive = require('./keep_alive.js')
 const mySecret = process.env['token']
@@ -18,12 +13,7 @@ let prefix = '=';
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.guilds.cache.forEach(guild=>console.log(`${guild.name}(${guild.id})`));
-       let scheduledMessage = new cron.CronJob('00 00 04 * * *', () => {
-         const guild = client.guilds.cache.get(process.env.grp2);
-         const channel = guild.channels.cache.get(process.env.channelID2);
-         channel.send('你各位別當死魚堆');
-        });
-        scheduledMessage.start()});
+});
 let date_ob = new Date();
 
 // current date
@@ -63,12 +53,34 @@ function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max-min) + min);
 }
 
+//client.on("messageCreate", async (message) => {
+ // if(!message.guild || message.author.bot) return
+  //let prefix = ":"
+  //let args = message.content.slice(prefix.length).trim().split(/ +/)
+  //let cmd = args.shift()?.toLowerCase()
+  //if(cmd = "test"){
+ // }
+//});
+
+//client.on("interactionCreate", async (interaction) => {
+//if(interaction.isContextMenuCommand()){
+  
+//}
+//});
+
 client.on("messageCreate", async (message) => {
   if(!message.guild || message.author.bot) return
   let prefix = ":"
-  let args = message.content.slice(prefix.length).trim().split(/ +/)
+  let args = message.content.slice(prefix.length).trim().split(/ /)
   let cmd = args.shift()?.toLowerCase()
-  if(cmd = "test"){
-    const enumValue = EnumResolvers.resolveButtonStyle('PRIMARY');
+  if(message.content.startsWith(`${prefix}newtextchannel`)){
+    message.guild.channels.create(cmd, {
+      type : "text"
+    })
   }
-})
+});
+
+     
+client.login(process.env.token).then(() => {
+    client.user.setPresence({ activities: [{ name: 'discord.js V14出了真開心, 全部重寫', type: 'PLAYING' }], status: 'idle' });
+});
